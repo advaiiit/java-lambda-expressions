3c0fea098e7319d638804afa1e762e05b152fb56
akamathe <advaitkamathe@gmail.com>
2022-12-26 18:59:04 +0530

    Adding Employee Class

--- src/com/advaitkamathe/PassingParameters.java
+++ src/com/advaitkamathe/PassingParameters.java
@@ -1,36 +1,9 @@
 package com.advaitkamathe;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class Employee {
-
-  private String name;
-  private int age;
-
-  public Employee(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-}
 public class PassingParameters {
 
   public static void main(String[] args) {
