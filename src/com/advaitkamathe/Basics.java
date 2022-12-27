89b5acd34e6bab7c5ada7d521ad99f2256beef5d
akamathe <advaitkamathe@gmail.com>
2022-12-27 16:24:36 +0530

    Adding comments in Basics.java

--- src/com/advaitkamathe/Basics.java
+++ src/com/advaitkamathe/Basics.java
@@ -4,10 +4,18 @@ public class Basics {
 
   public static void main(String[] args) {
 
-    // Lambda expressions provide us an easier way to work with interfaces which have only one method
-    // They are often used in places where we use anonymous classes
-    // They are more convenient and concise way of writing anonymous classes
+    // Lambda expressions provide us an easier way to work with methods of functional interfaces.
+    // Functional Interfaces only contains one abstract method but can have multiple default and static methods
+    // Ex.
+    //    Runnable -> run()
+    //    Callable -> call()
+    //    Comparable -> compareTo()
+    //    Comparator -> compare()
 
+    // Lambda expressions are often used in places where we use anonymous methods
+    // They are more convenient and concise way of writing anonymous methods
+
+    // Anonymous method
     new Thread(new Runnable() {
       @Override
       public void run() {
@@ -16,10 +24,11 @@ public class Basics {
     }).start();
 
     // Each lambda expression has got 3 parts:
-    // 1) Argument List
-    // 2) Arrow token
-    // 3) Body
+    // 1) Argument List ()
+    // 2) Arrow token   ->
+    // 3) Body          {}
 
+    // Lambda Expression
     new Thread(() -> System.out.println("Printing from lambda expression")).start();
   }
 }
