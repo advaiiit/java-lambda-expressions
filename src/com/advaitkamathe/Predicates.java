527ff435649b34d18fc04a3781250fa391fdc24e
akamathe <advaitkamathe@gmail.com>
2022-12-27 22:33:43 +0530

    Minor changes in Predicates

--- src/com/advaitkamathe/Predicates.java
+++ src/com/advaitkamathe/Predicates.java
@@ -2,14 +2,15 @@ package com.advaitkamathe;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Predicates {
 
-  private static void printEmployeesByAge(List<Employee> employees,
-                                          String ageText,
-                                          Predicate<Employee> ageCondition) {
+  // Predicate functional interface is used for conditional check
+
+  private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
 
     System.out.println(ageText);
     for (Employee employee : employees) {
@@ -32,5 +33,18 @@ public class Predicates {
 
     printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
     printEmployeesByAge(employees, "Employees below 30", employee -> employee.getAge() < 30);
+
+    Predicate<Integer> predicate = (t) -> {
+      if (t % 2 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    System.out.println(predicate.test(8));
+
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+    list.stream().filter(predicate).forEach(t -> System.out.println(t + "\t"));
   }
 }
